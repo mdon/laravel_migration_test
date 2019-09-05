94ced972b13dd5866da94128476dad7da8d3ccd7
vitali <v.kostev@gmail.com>
2019-09-05 15:09:55 +0300

    Migrated db

--- database/migrations/2019_09_05_114937_create_test_table_table.php
+++ database/migrations/2019_09_05_114937_create_test_table_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePeopleTable extends Migration
+class CreateTestTableTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,8 @@ class CreatePeopleTable extends Migration
      */
     public function up()
     {
-        Schema::create('people', function (Blueprint $table) {
+        Schema::create('testTable', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('phone');
-            $table->string('email');
-            $table->string('city');
-            $table->string('test');
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ class CreatePeopleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('people');
+        Schema::dropIfExists('testTable');
     }
 }
